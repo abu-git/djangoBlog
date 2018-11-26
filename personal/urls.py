66f1f543000235ac1d6f4ab645897a5a8e6762da
abu-git <abu.sammie@gmail.com>
2018-11-26 21:47:43 +0200

    Task 15 urls fixed

--- personal/urls.py
+++ personal/urls.py
@@ -5,5 +5,5 @@ from .views import HomePageView, CalculatorPageView, JSObjectPageView
 urlpatterns = [
 	path('calculator/', CalculatorPageView.as_view(), name='calculator'),
 	path('jsObjects/', JSObjectPageView.as_view(), name='jsObjects'),
-	path('home/', HomePageView.as_view(), name='home')
+	path('', HomePageView.as_view(), name='home')
 ]
