84b30d895547da79affcc49a449ce96207c42660
abu-git <abu.sammie@gmail.com>
2018-11-28 00:03:10 +0200

    Task 16 done...blog works

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,10 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
+	signature = models.CharField(max_length=140, default='together we stand, divided we fall')
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 	def __str__(self):
 		return self.title
